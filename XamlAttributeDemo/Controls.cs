4b1ac86095f51b472f8219765f86cf9a5df1a38d
Mike Christiansen <michael.a.christiansen10@gmail.com>
2023-08-09 16:25:16 -0400

    Initial commit

---
+++ XamlAttributeDemo/Controls.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using JetBrains.Annotations;
+
+namespace XamlAttributeDemo;
+
+public class ColumnLikeControl : DependencyObject
+{
+    public static readonly DependencyProperty BindingProperty = 
+        DependencyProperty.Register(nameof(Binding), typeof(BindingBase), typeof(ColumnLikeControl));
+
+    [XamlItemBindingOfItemsControl]
+    public BindingBase? Binding
+    {
+        get => this.GetValue(BindingProperty) as BindingBase;
+        set => this.SetValue(BindingProperty, value);
+    }
+}
+
+public class DataGridLikeControl : ItemsControl
+{
+    public ObservableCollection<ColumnLikeControl> Columns { get; } = new();
+}
+
+public static class AttachedProperties
+{
+    public static readonly DependencyProperty AttachedColumnsProperty = DependencyProperty.RegisterAttached(
+        "AttachedColumns",
+        typeof(ObservableCollection<ColumnLikeControl>),
+        typeof(AttachedProperties),
+        new FrameworkPropertyMetadata()
+    );
+    [AttachedPropertyBrowsableForType(typeof(DataGridLikeControl))]
+    public static ObservableCollection<ColumnLikeControl>? GetAttachedColumns(DataGridLikeControl target)
+        => target.GetValue(AttachedColumnsProperty) as ObservableCollection<ColumnLikeControl>;
+    public static void SetAttachedColumns(DataGridLikeControl target, ObservableCollection<ColumnLikeControl> value)
+        => target.SetValue(AttachedColumnsProperty, value);
+}
+
---
+++ XamlAttributeDemo/Models.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.ObjectModel;
+
+namespace XamlAttributeDemo;
+
+public class AppViewModel
+{
+    public ObservableCollection<Item> Items { get; } = new();
+}
+
+public class Item
+{
+    public int Value { get; } = Random.Shared.Next(0, 100);
+}
